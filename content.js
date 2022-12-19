2680710aa06ca686daaab6c63b5836ab1ca21a89
Brian <vojtko.brian@yahoo.com>
2022-12-19 17:10:31 -0500

    updated parameters

--- content.js
+++ content.js
@@ -1,8 +1,8 @@
 document.getElementById('form').addEventListener('submit', (event) => {
   event.preventDefault();
 
-  const keywords = document.getElementById('keywords').value;
-  const years_of_experience = document.getElementById('years_of_experience').value;
+  const keywords = document.getElementById('tech-stack').value;
+  const years_of_experience = document.getElementById('years-of-experience').value;  
 
   // Send a message to the content script with the form data
   chrome.tabs.sendMessage(tab.id, {
